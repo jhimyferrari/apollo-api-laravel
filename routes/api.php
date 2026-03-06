6998db9f1815eb972a98d02165c7ad895ef1eced
Jhimy Ferrari <souzaferrarik@gmail.com>
2026-03-06 16:13:56 -0300

    chore: change organization

--- routes/api.php
+++ routes/api.php
@@ -6,23 +6,26 @@ use App\Http\Controllers\api\v1\UserController as UserControllerV1;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::prefix('v1')
     ->name('v1.')
     ->group(callback: function () {
 
-        Route::post('/login', [LoginControllerV1::class, 'login'])->name('login');
-
+        // Non authenticated routes
         Route::group(['as' => 'organizations.'], function () {
             Route::post('/organizations', [OrganizationControllerV1::class, 'store'])->name('store');
         });
 
-        Route::middleware('auth')->group(function () {
+        Route::post('/login', [LoginControllerV1::class, 'login'])->name('login');
+
+        // Authenticated routes
+        Route::middleware('auth:sanctum')->group(function () {
             Route::group(['as' => 'users.'], function () {
                 Route::post('/users', [UserControllerV1::class, 'store'])->name('store');
+
             });
 
         });
