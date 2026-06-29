cd98523e6bb5a489cd17ef71327b64e77afc364e
Jhimy Ferrari <souzaferrarik@gmail.com>
2026-06-29 19:29:51 -0300

    fix: correct behavior of `ignoringId`

--- app/Actions/ValidateDuplicateField.php
+++ app/Actions/ValidateDuplicateField.php
@@ -7,21 +7,41 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
 use RuntimeException;
 
+/**
+ * Action for validate if exist some duplicate value in a field on database
+ *
+ * When using in a Auth context, it will be scopedBy `OrganizationScope`
+ * based on the Model that is passed.
+ * When using in a non-Auth context, you may declare the organizationId input
+ *
+ * @throws DuplicateFieldException
+ * @throws RuntimeException
+ */
 class ValidateDuplicateField
 {
-    public function execute(Model $model, string $field, mixed $value, string $organizationId, ?string $ignoredId = null)
+    /**
+     * @throws DuplicateFieldException
+     * @throws RuntimeException
+     */
+    public function execute(Model $model, string $field, mixed $value, ?string $organizationId = null, ?string $ignoredId = null)
     {
         if (! Schema::hasColumn($model->getTable(), $field)) {
             throw new RuntimeException("Attribute `$field` does not exist in ".class_basename($model));
         }
-        $findResults = $model->newQuery()->where($field, $value)->where('organization_id', $organizationId);
+        $findResults = $model->newQuery()->where($field, $value);
 
+        if ($organizationId !== null) {
+            if (! Schema::hasColumn($model->getTable(), 'organization_id')) {
+                throw new RuntimeException('Attribute `organization_id` does not exist in '.class_basename($model));
+            }
+            $findResults->where('organization_id', $organizationId);
+        }
         if ($ignoredId !== null) {
-            $findResults->where('id', '=!', $ignoredId);
+            $findResults->where('id', '!=', $ignoredId);
         }
 
         if ($findResults->exists()) {
-            throw new DuplicateFieldException(class_basename($model)." $field `$value` already exists");
+            throw new DuplicateFieldException(class_basename($model)." $field `$value` already exist");
         }
 
     }
---
+++ tests/Feature/Actions/ValidateDuplicateFieldTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Actions\ValidateDuplicateField;
+use App\Exceptions\DuplicateFieldException;
+use App\Models\Client;
+use App\Models\Organization;
+use App\Models\Uf;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+describe('ValidateDuplicateField', function () {
+    it('throw an error when pass a non existed field', function () {
+        expect(fn () => app(ValidateDuplicateField::class)
+            ->execute(new Client, 'non_existedField', 'value'))
+            ->toThrow(RuntimeException::class, 'Attribute `non_existedField` does not exist in Client');
+    });
+    it('throw an error when pass a organization_id when does not exist in model', function () {
+        expect(fn () => app(ValidateDuplicateField::class)
+            ->execute(new Uf, 'name', 'value', '1'))
+            ->toThrow(RuntimeException::class, 'Attribute `organization_id` does not exist in Uf');
+    });
+
+    it('throw an error when pass duplicated values', function () {
+        $organization = Organization::factory()->create();
+        expect(fn () => app(ValidateDuplicateField::class)
+            ->execute(new Organization, 'document', $organization->document))
+            ->toThrow(DuplicateFieldException::class, 'Organization document `'.$organization->document.'` already exist');
+    });
+    it('not throw an error when using unique value  ', function () {
+        expect(fn () => app(ValidateDuplicateField::class)
+            ->execute(new Client, 'document', '12345678'))
+            ->not
+            ->toThrow(DuplicateFieldException::class);
+    });
+    it('not throw an error when using duplicated values from differents organization', function () {
+        $organization = Organization::factory()->create();
+        $document = Client::factory()->create()->document;
+        expect(fn () => app(ValidateDuplicateField::class)
+            ->execute(new Client, 'document', $document, $organization->id))
+            ->not
+            ->toThrow(DuplicateFieldException::class);
+    });
+    it('throw an error when using duplicated values from same organizations  ', function () {
+        $client = Client::factory()->create();
+        expect(fn () => app(ValidateDuplicateField::class)
+            ->execute(new Client, 'document', $client->document, $client->organization_id))
+            ->toThrow(DuplicateFieldException::class);
+    });
+});
